7d506ece44825850744bcd40e409b70aafc0d978
UnikMask <alexandre.guerville@hotmail.com>
2024-03-13 23:25:09 +0100

    feat(model): Define endpoints for feed and posts

--- model/endpoints.go
+++ model/endpoints.go
@@ -4,4 +4,6 @@ const (
 	ENDPOINT_FORMS_VERIFY = "/api/forms/verify"
 	ENDPOINT_FORMS        = "/api/forms"
 	ENDPOINT_USERS        = "/api/users"
+	ENDPOINT_POSTS        = "/api/posts"
+	ENDPOINT_FEED         = "/api/feed"
 )
