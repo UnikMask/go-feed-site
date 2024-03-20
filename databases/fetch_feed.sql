6c4e802759b633b204e286a32f8c02385ad54752
UnikMask <alexandre.guerville@hotmail.com>
2024-03-20 19:00:37 +0100

    feat(feed): Basic fetch feed query

---
+++ databases/fetch_feed.sql
@@ -0,0 +1,24 @@
+SELECT
+    p.ROWID,
+    u.ROWID,
+    u.username,
+    p.contents,
+    p.posted_at,
+    COUNT(l.post_id) AS likes,
+    CASE
+        WHEN SUM(l.user_id = 1) THEN 1
+        ELSE 0
+    END AS liked,
+    CASE 
+        WHEN COUNT(f.follower) THEN 1
+        ELSE 0
+    END AS followed
+FROM
+    Posts AS p
+JOIN Users AS u
+    ON p.user_id = u.ROWID
+LEFT JOIN Likes AS l
+    ON l.post_id = p.ROWID
+LEFT JOIN Follows AS f on f.followee = u.ROWID AND f.follower = 1
+GROUP BY 
+    p.ROWID, f.follower;
