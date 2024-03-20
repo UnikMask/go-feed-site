7a6a5a2f6c2dad4826819208358cc729e8a4ccfd
UnikMask <alexandre.guerville@hotmail.com>
2024-03-20 21:33:31 +0100

    feat(feed): Get posts from feed query

---
+++ posts/feed.go
@@ -0,0 +1,24 @@
+package posts
+
+import (
+	"github.com/UnikMask/gofeedsite/databases"
+	"github.com/UnikMask/gofeedsite/model"
+)
+
+func GetPosts(user_id int) ([]model.Post, error) {
+	rows, err := databases.Query("databases/fetch_feed.sql", user_id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	posts := []model.Post{}
+	for hasNext := true; hasNext; {
+		p := model.Post{}
+		hasNext, err = rows.ScanNext(&p.Id, &p.UserId, &p.Username, &p.Content, &p.PostedAt, &p.Likes, &p.Liked, &p.Followed)
+		if err == nil {
+			posts = append(posts, p)
+		}
+	}
+	return posts, err
+}
