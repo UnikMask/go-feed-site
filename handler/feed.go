db74d92adb44036441b331eec8c7eb9f89e26c42
UnikMask <alexandre.guerville@hotmail.com>
2024-03-20 21:34:21 +0100

    feat(feed): Complete hypermedia endpoint to fetch feed

--- handler/feed.go
+++ handler/feed.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"fmt"
 	"strconv"
 
 	"github.com/UnikMask/gofeedsite/auth"
 	"github.com/UnikMask/gofeedsite/model"
+	"github.com/UnikMask/gofeedsite/posts"
+	"github.com/UnikMask/gofeedsite/view/components"
 	"github.com/UnikMask/gofeedsite/view/layout"
 	"github.com/labstack/echo/v4"
 )
@@ -15,6 +18,7 @@ func AttachFeedHandlers(app *echo.Echo) {
 
 	api := app.Group(model.ENDPOINT_FEED)
 	api.Use(auth.StrictAuthMiddleware)
+	api.GET("/", HandleGetFeed)
 }
 
 func HandleFeedPage(c echo.Context) error {
@@ -26,5 +30,11 @@ func HandleGetFeed(c echo.Context) error {
 	if err != nil || page < 0 {
 		page = 0
 	}
-	return nil
+	id := auth.GetUserFromContextOrNone(c.Request().Context()).Id
+	feed, err := posts.GetPosts(id)
+	if err != nil {
+		fmt.Printf("Failed to get user feed for user %d: %v", id, err)
+		return render(c, layout.PostError("Failed to get posts - try again later!"))
+	}
+	return render(c, components.FeedSegment(0, feed))
 }
